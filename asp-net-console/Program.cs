12be40d3a829af3d5601646144a52aaae4c1f817
Вячеслав Матюхин <matuhinv27@gmail.com>
2023-10-30 22:04:34 +0300

    create console app

---
+++ asp-net-console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace asp_net_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
